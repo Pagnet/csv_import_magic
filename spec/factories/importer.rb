e904b2249fe9ce44a1747529a9fdaa0aeecc5b79
Fernando Bellincanta <ervalhous@hotmail.com>
2019-07-09 09:22:29 -0300

    Changing attributes to new FactoryBot dynamic format

--- spec/factories/importer.rb
+++ spec/factories/importer.rb
@@ -3,8 +3,8 @@ include ActionDispatch::TestProcess
 FactoryBot.define do
   factory :importer do
     attachment { fixture_file_upload(File.join(ENGINE_RAILS_ROOT, 'spec/fixtures/companies.csv')) }
-    source 'company'
-    columns %w(name number city state)
+    source { 'company' }
+    columns { %w(name number city state) }
     importable { build(:user) }
   end
 end
--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :user do
-    name 'foo'
-    email 'foo@bar.com'
+    name { 'foo' }
+    email { 'foo@bar.com' }
   end
 end
